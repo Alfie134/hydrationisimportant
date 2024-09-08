d73bc84d28a62a280edf1c0e21baacf5ffd72c72
Ea Overvad <ea@0vervad.dk>
2024-09-08 16:33:03 +0200

    Initial commit to new repository

---
+++ AmbulanceOptimization/Model/Municipality.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmbulanceOptimization.Model
+{
+    public class Municipality
+    {
+        public int MunicipalityID { get; set; }  // Primær nøgle
+        public string Name { get; set; }  // Kommunens navn (f.eks. "Odense Kommune")
+    }
+
+}
