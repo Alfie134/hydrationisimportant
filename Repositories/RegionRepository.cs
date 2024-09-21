697a9b8a25f12ce6438bf717899d42106a6b370f
Alberte Øgendahl <alberteoegendahl@gmail.com>
2024-09-21 16:49:27 +0200

    Repository, interface og test til Region tilføjet.

---
+++ Repositories/RegionRepository.cs
@@ -0,0 +1,58 @@
+﻿using Configuration;
+using Microsoft.Data.SqlClient;
+using Models;
+using Repositories.Interfaces;
+
+namespace Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly string _connectionString = new AppConfig().ConnectionString;
+
+
+        public IEnumerable<Region> GetAll()
+        {
+            var regions = new List<Region>();
+            string query = "SELECT * FROM Region";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string? itSystem = reader.IsDBNull(2) ? null : reader.GetString(2);
+                        regions.Add(new Region((int)reader["Id"], (string)reader["Name"], itSystem));
+                    }
+                }
+            }
+            return regions;
+        }
+
+
+        public Region GetById(int id)
+        {
+            Region region = null;
+            string query = "SELECT * FROM Region WHERE Id = @Id";
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Id", id);
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        string? itSystem = reader.IsDBNull(2) ? null : reader.GetString(2);
+                        region = new Region((int)reader["Id"], (string)reader["Name"], itSystem);
+                    }
+                }
+                return region;
+            }
+        }
+    }
+}
