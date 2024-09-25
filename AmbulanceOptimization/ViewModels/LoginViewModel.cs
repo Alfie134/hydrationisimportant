2e7af67a1f77621222969689d5bb19b83c42981c
Ea Overvad <ea@0vervad.dk>
2024-09-25 17:40:51 +0200

    Updated

---
+++ AmbulanceOptimization/ViewModels/LoginViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AmbulanceOptimization.Controllers;
+
+namespace AmbulanceOptimization
+{
+    class LoginViewModel
+    {
+        public string UserName; //Databindet til username textbox
+        public string Password; //Databindet til password textbox
+        private LoginController _loginController;
+
+        LoginViewModel()
+        {
+            _loginController = new LoginController();   
+        }
+
+        //LoginCommand //commandBindet til en loginknappen 
+
+        public void Login()
+        {
+            //check om der findes en bruger med kombinationen af brugernavn og adgangskode 
+            // videresend Bruger til MenuWindw 
+        }
+
+    }
+}
