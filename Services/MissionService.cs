e63fc7ab375eb6860ebcb1d8528171789434e73f
Alberte Øgendahl <alberteoegendahl@gmail.com>
2024-10-08 17:36:45 +0200

    Opdateret opgavebankviewmodel og repos; korrekt data vises.

--- Services/MissionService.cs
+++ Services/MissionService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using Configuration;
+﻿using Configuration;
 using Microsoft.Data.SqlClient;
 using Models;
 using Repositories;
